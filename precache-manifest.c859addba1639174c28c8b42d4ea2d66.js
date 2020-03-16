d674da0f8cea4120d974d5a674b74a08f3968cb9
Coder-Aht007 <60315450+Coder-Aht007@users.noreply.github.com>
2020-03-17 01:39:37 +0500

    Updates

---
+++ precache-manifest.c859addba1639174c28c8b42d4ea2d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33b95e5081ba7815ac5742f4b0e8bed",
+    "url": "/MERN_Stack_ExerciseTrackerApp/index.html"
+  },
+  {
+    "revision": "7a61015b71df1d5a82bb",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "7eddae1da4826ff5bec4",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a61015b71df1d5a82bb",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/js/2.927740d3.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/js/2.927740d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eddae1da4826ff5bec4",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/js/main.47dda626.chunk.js"
+  },
+  {
+    "revision": "77e48edd9558d2d38ba1",
+    "url": "/MERN_Stack_ExerciseTrackerApp/static/js/runtime-main.2bcd9e2c.js"
+  }
+]);
